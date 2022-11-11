ff5f263fd2b16e29f3d067f27cc7da8d043fe599
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-11-11 20:13:49 +0300

    fix: updated logic in Header comp.

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,9 +1,12 @@
+import { useAppDispatch } from "app/hooks";
 import Link from "components/link/Link";
+import { setIsLogin } from "features/user/UserSlice";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { LinksNavWrapper, HeadersWrapper } from "./Header.style";
 
 const Header = () => {
+  const dispatch = useAppDispatch();
   return (
     <>
       <HeadersWrapper>
@@ -11,7 +14,9 @@ const Header = () => {
           <Link to="/edit-profile" text="edit profile" />
           <Link to="/boards" text="create new board" />
           <Link to="#" text="select language" />
-          <Link to="/" text="sing out" />
+          <div onClick={() => dispatch(setIsLogin(false))}>
+            <Link to="/" text="sing out" />
+          </div>
         </LinksNavWrapper>
       </HeadersWrapper>
       <Outlet />
